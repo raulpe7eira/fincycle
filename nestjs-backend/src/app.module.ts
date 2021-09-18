e66f8b4436716ee0de12d48a63ab2a253413196c
Raul Pereira <raul.pereira@gmail.com>
2021-09-18 14:44:20 -0300

    class-01: config docker nestjs-backend project

--- nestjs-backend/src/app.module.ts
+++ nestjs-backend/src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    SequelizeModule.forRoot({
+      dialect: process.env.DB_CONNECTION as any,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      models: [],
+      autoLoadModels: true,
+      synchronize: true,
+      sync: {
+        alter: true,
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
